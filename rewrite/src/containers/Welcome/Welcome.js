e01ab0f4129c437df46e5164afd98f11062831e4
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-09-14 23:07:22 -0400

    working on routing and setup

--- rewrite/src/containers/Welcome/Welcome.js
+++ rewrite/src/containers/Welcome/Welcome.js
@@ -2,27 +2,28 @@ import React from 'react';
 import '../../sass/main.scss';
 import { motion } from "framer-motion";
 import Menu from '../../components/Menu/Menu';
-import More from '../../components/More/More';
+import Button from '../../components/Button/Button';
 
 const Welcome = () => {
   return (
-    <div className="Welcome">
-      <span className="background">
-        &nbsp;&nbsp;&nbsp;Stéfan 
-        <br /> 
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Martin
-      </span>
-      <Menu />
-      <div className="introduction">
-      <div className="introduction__main">
-        <span className="introduction__text">Hi </span>
-        <span className="introduction__text">I'm </span>
-        <span className="introduction__text">Stéfan</span>
-        
+
+      <div className="Welcome">
+        <span className="background">
+          &nbsp;&nbsp;&nbsp;Stéfan 
+          <br /> 
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Martin
+        </span>
+        <Menu />
+        <div className="introduction">
+        <div className="introduction__main">
+          <span className="introduction__text">Hi </span>
+          <span className="introduction__text">I'm </span>
+          <span className="introduction__text">Stéfan</span>
+          
+        </div>
+          
+        </div>
       </div>
-        <More />
-      </div>
-    </div>
   );
 }
 
