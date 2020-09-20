3a2949c6446e3b03a359ae1ae212010673763c4d
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-09-20 14:47:29 -0400

    making the cards more interactive

--- rewrite/src/components/card/card.js
+++ rewrite/src/components/card/card.js
@@ -6,25 +6,13 @@ import Button from '../Button/Button';
 
 export const Card = (props) => {
 
-  const [shown , setShow] = useState(false);
-
-
   return (
-    <div className='card'
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}>
+    <div className='card'>
       <div className='card__content'>
         <h3 className='card__content--title'>{props.title}</h3>
         <h3 className='card__content--comp'>{props.company}</h3>
         <p className="card__content--description">{props.desc}</p>
-
-
-        {shown && (
-          <Button className='card__Button'></Button>
-        )}
-        
       </div>
-
     </div>
   );
 }
