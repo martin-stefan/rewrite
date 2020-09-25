c102d4189a1271d710e2a976815e6c4f7e8bedab
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-09-25 00:10:56 -0400

    starting work page

--- rewrite/src/containers/Work/Work.js
+++ rewrite/src/containers/Work/Work.js
@@ -1,40 +1,43 @@
 // this will contain my work experience so far
 
-// page that will contain all of my projects
-
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import Aux from '../../Auxilliary';
 
 const Work = (props) => {
   return (
-    <div className="work">
-      <header>Work Experience</header>
-      <div  className="work__list">
-        <Banner 
-          title="Programming Instructor"
-          company="Juni Learning"
-          time="June 2020 - Present"
-          desc="Currently I am working with Juni Learning as a programming instructor. The two courses
-          that I teach are Python Level 1 and JavaScript Level 1. "
-          img=''
-        />
-        <Banner 
-          title="Stock Worker"
-          company="Hayward's Ice Cream Shop"
-          time="June 2019 - May 2020"
-          desc="I did all of this stuff"
-          img=''
-        />
-        <Banner 
-          title="Birthday Host"
-          company="Launch Trampoline Park"
-          time="June 2017 - July 2019"
-          desc="I did all of this stuff"
-          img=''
-        />
+    <Aux>
 
+      <h1 className="pageTitle">Work Experience</h1>
+
+      <div className="work">
+        <div  className="work__list">
+          <Banner 
+            title="Programming Instructor"
+            company="Juni Learning"
+            time="June 2020 - Present"
+            desc="Currently I am working with Juni Learning as a programming instructor. The two courses
+            that I teach are Python Level 1 and JavaScript Level 1. "
+            img=''
+          />
+          <Banner 
+            title="Stock Worker"
+            company="Hayward's Ice Cream Shop"
+            time="June 2019 - May 2020"
+            desc="I did all of this stuff"
+            img=''
+          />
+          <Banner 
+            title="Birthday Host"
+            company="Launch Trampoline Park"
+            time="June 2017 - July 2019"
+            desc="I did all of this stuff"
+            img=''
+          />
+
+        </div>
       </div>
-    </div>
+    </Aux>
   );
 }
 
