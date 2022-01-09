edae2bdf3807f7893795bfcebf41c57b9b5075ef
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2022-01-09 12:09:30 -0500

    initial changes

--- rewrite/src/containers/Welcome/Welcome.js
+++ rewrite/src/containers/Welcome/Welcome.js
@@ -15,13 +15,13 @@ const Welcome = () => {
         </div>
 
         <p className="intro__desc">
-          I am a junior at Rensselaer Polytechnic Institute pursuing 
+          I am a rising senior at Rensselaer Polytechnic Institute pursuing 
           my bachelor's degree in Information Technology and Web Science
           looking for an internship/co-op for spring and summer 2022. 
         </p>
 
         <div className="intro__btn">
-          <a href="#projects" className="intro__btn--link">View my experience</a>
+          <a href="#projects" className="intro__btn--link">Learn More</a>
         </div>
       </div>
     </div>
