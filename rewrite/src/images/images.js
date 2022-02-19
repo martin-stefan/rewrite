0666ca312f08d389250db2f5093e5aa8da508307
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2022-02-19 18:20:33 -0500

    removing udemy project images

--- rewrite/src/images/images.js
+++ rewrite/src/images/images.js
@@ -1,4 +1,3 @@
-import budgetyOne from '../images/Budgety/one.jpg';
 import createorder from "../images/DeFazio's/createorder.jpg";
 import details from "../images/DeFazio's/details.jpg";
 import home from "../images/DeFazio's/home.jpg";
@@ -6,17 +5,15 @@ import menuCat from "../images/DeFazio's/menuCat.jpg";
 import menuItems from "../images/DeFazio's/menuItems.jpg";
 import signin from "../images/DeFazio's/signin.jpg";
 
+import feed from "../images"
+
 
 
 const images = {
-  "Budgety": [budgetyOne],
   "DeFazio's": [createorder, details, home, menuCat, menuItems, signin],
-  "Natours": [],
   "Pizzi": [],
   "Spects": [],
   "The Loop": [],
-  "Forkify": [],
-
 }
 
 export default images;
