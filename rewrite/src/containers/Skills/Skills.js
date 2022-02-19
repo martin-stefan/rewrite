85ef0d769a650fe0a8b6e261b10a9f04ff15dfb7
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2022-02-19 17:52:40 -0500

    removing skills images

--- rewrite/src/containers/Skills/Skills.js
+++ rewrite/src/containers/Skills/Skills.js
@@ -1,12 +1,5 @@
 import React from 'react';
 
-import htmlLogo from '../../logos/html.png';
-import sassLogo from '../../logos/sass.png';
-import reactLogo from '../../logos/react.png';
-import jsLogo from '../../logos/js.png';
-import cplusLogo from '../../logos/cplus.png';
-import pythonLogo from '../../logos/python.png';
-
 export const Skills = () => {
   return (
     <div>
@@ -16,37 +9,29 @@ export const Skills = () => {
           <div className="skills__box">
 
             <div className="skills__box__item">
-              {/* <img src={htmlLogo} className="skills__box__item--img"/> */}
               <p className="skills__box__item--title">HTML</p>
             </div>          
 
             <div className="skills__box__item">
-              {/* <img src={sassLogo} className="skills__box__item--img"/> */}
               <p className="skills__box__item--title">CSS & SCSS</p>
             </div>
 
             <div className="skills__box__item">
-              {/* <img src={jsLogo} className="skills__box__item--img"/> */}
               <p className="skills__box__item--title">JavaScript</p>
             </div>
             
             <div className="skills__box__item">
-              {/* <img src={reactLogo} className="skills__box__item--img"/> */}
-              <p className="skills__box__item--title">React.js</p>
+              <p className="skills__box__item--title">React</p>
             </div>
 
             <div className="skills__box__item">
-              {/* <img src={pythonLogo} className="skills__box__item--img"/> */}
               <p className="skills__box__item--title">Python</p>
-      
             </div>
 
             <div className="skills__box__item">
-              {/* <img src={cplusLogo} className="skills__box__item--img"/> */}
-              <p className="skills__box__item--title">C++</p>
-
+              <p className="skills__box__item--title">React Native</p>
             </div>
-
+            
             <div className="skills__box__item">
               <p className="skills__box__item--title">Firebase</p>
             </div>
@@ -58,13 +43,7 @@ export const Skills = () => {
             <div className="skills__box__item">
               <p className="skills__box__item--title">Figma</p>
             </div>
-
-            <div className="skills__box__item">
-              <p className="skills__box__item--title">React Native</p>
-            </div>
-          
           </div>
-
         </div>
       </div>
   );
